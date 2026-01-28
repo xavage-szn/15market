d7339bee51a0da44d3b4f4b9f3d5fd36bd53e4b0
xavage-szn <admin@15market.online>
2026-01-28 19:55:29 +0100

    chore: project cleanup for production

--- arc_keeper/check_bal.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { ethers } = require('ethers');
-const p = new ethers.JsonRpcProvider('https://rpc.quicknode.testnet.arc.network');
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, p);
-p.getBalance(wallet.address)
-    .then(b => console.log('KEEPER ADDRESS:', wallet.address, '\nBALANCE:', ethers.formatEther(b), 'ARC/USDC'))
-    .catch(console.error);
--- calc_pda.js
+++
@@ -1,8 +0,0 @@
-const { PublicKey } = require('@solana/web3.js');
-const oldPid = new PublicKey('7yZWzWwkc4JYkum1FSL37Peb9iKK9vyAciPbu3QzE1cP');
-const [oldTreasury] = PublicKey.findProgramAddressSync([Buffer.from('treasury')], oldPid);
-console.log('Old Treasury PDA:', oldTreasury.toBase58());
-
-const newPid = new PublicKey('9bK6W6uAS2xC9Xdw6fcMgHH1BwLp4gJDKj5dwciF8CPe');
-const [newTreasury] = PublicKey.findProgramAddressSync([Buffer.from('treasury')], newPid);
-console.log('New Treasury PDA:', newTreasury.toBase58());
