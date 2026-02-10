a4b763b059bb8e8e91a3e3ca372ff960a644b148
xavage-szn <admin@15market.online>
2026-02-10 17:30:11 +0100

    fix: restore ParaProvider initialization using paraClientConfig to avoid SDK crash

--- 15market-admin/src/providers/ParaProvider.jsx
+++ 15market-admin/src/providers/ParaProvider.jsx
@@ -1,14 +1,17 @@
 import { ParaProvider } from "@getpara/react-sdk";
 import "@getpara/react-sdk/styles.css";
 import { arcTestnet, projectId, ARC_RPC } from "../constants";
-import { para, queryClient } from "../paraClient";
+import { para, queryClient, paraApiKey, paraEnv } from "../paraClient";
 
 export function AppParaProvider({ children }) {
     const activeProjectId = projectId || "4aebd2ef806c541b6aaf003da2930c58";
 
     return (
         <ParaProvider
-            para={para}
+            paraClientConfig={{
+                apiKey: paraApiKey,
+                env: paraEnv
+            }}
             queryClient={queryClient}
             externalWalletConfig={{
                 appName: "15market",
